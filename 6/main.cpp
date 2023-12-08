2fe7e31a26b922bbbfdd0b8c8e7cfa3eee4c2e20
christian <cmmw@gmx.net>
2023-12-08 19:09:44 +0100

    day 6 part 1

---
+++ 6/main.cpp
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <vector>
+
+using Races = std::vector<std::pair<int64_t, int64_t>>;
+
+Races parseInput() {
+    Races races;
+    std::ifstream ifs("../../6/input.txt");
+    std::string line;
+    std::getline(ifs, line);
+    std::stringstream ss(line);
+    std::string tmp;
+    ss >> tmp;
+    int64_t time;
+    while (ss >> time)
+        races.emplace_back(time, 0);
+    std::getline(ifs, line);
+    ss = std::stringstream(line);
+    ss >> tmp;
+    int32_t i{};
+    int64_t distance;
+    while (ss >> distance)
+        races[i++].second = distance;
+    return races;
+}
+
+void part1() {
+    Races races = parseInput();
+    int64_t sum = 1;
+    for (const auto& [time, dist] : races) {
+        int32_t cnt{};
+        for (int i = 1; i < time; i++)
+            cnt += (time - i) * i > dist;
+        sum *= cnt;
+    }
+    std::cout << sum << std::endl;
+}
+
+void part2() {
+}
+
+int main() {
+    part1();
+    part2();
+    return 0;
+}
