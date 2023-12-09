bf0534bcb2028dd2e9160772d023758160dea53e
christian <cmmw@gmx.net>
2023-12-09 11:21:26 +0100

    day 9 part 2

--- 9/main.cpp
+++ 9/main.cpp
@@ -42,6 +42,26 @@ void part1() {
 }
 
 void part2() {
+    std::ifstream ifs("../../9/input.txt");
+    std::string line;
+    int64_t sum{};
+    while (std::getline(ifs, line)) {
+        Layers layers{parseLine(line)};
+        while (std::ranges::any_of(layers.back(), [](int32_t v) { return v != 0; })) {
+            const auto& prev = layers.back();
+            Layer next;
+            assert(prev.size() > 1);
+            next.reserve(prev.size() - 1);
+            for (int i = 0; i < prev.size() - 1; i++)
+                next.push_back(prev[i + 1] - prev[i]);
+            layers.push_back(next);
+        }
+        int64_t x{};
+        for (auto i = static_cast<int32_t>(layers.size() - 1); i >= 0; i--)
+            x = layers[i].front() - x;
+        sum += x;
+    }
+    std::cout << sum << std::endl;
 }
 
 int main() {
