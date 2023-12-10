6af3a260c512b45498bbcff2abc9c3cba9b3538d
christian <cmmw@gmx.net>
2023-12-10 18:45:15 +0100

    day 10 part 2

--- 10/main.cpp
+++ 10/main.cpp
@@ -80,6 +80,7 @@ Vec first(const Map& map, const Vec& s, int32_t width, int32_t height) {
             }
         }
     }
+    return {};
 }
 
 void part1() {
@@ -98,8 +99,113 @@ void part1() {
     std::cout << (cnt >> 1) << std::endl;
 }
 
+void replaceS(Map& map, const Vec& s, int32_t width, int32_t height) {
+    char c1{}, c2{};
+    for (auto y = std::max(0, s.second - 1); y <= std::min(s.second + 1, height - 1); y++) {
+        for (auto x = std::max(0, s.first - 1); x <= std::min(s.first + 1, width - 1); x++) {
+            if (x == s.first || y == s.second) {
+                auto c = map[x + y * width];
+                if (c != '.' && c != 'S') {
+                    const Vec d{x - s.first, y - s.second};
+                    auto r = allowed.at(d);
+                    if (std::ranges::find(r, c) != r.end()) {
+                        if (d.first < 0)
+                            c = 'W';
+                        else if (d.first > 0)
+                            c = 'E';
+                        else if (d.second < 0)
+                            c = 'N';
+                        else if (d.second > 0)
+                            c = 'S';
+                        c1 == 0 ? c1 = c : c2 = c;
+                    }
+                }
+            }
+        }
+    }
+    if (c1 > c2)
+        std::swap(c1, c2);
+    char sym;
+    if (c1 == 'E') {
+        switch (c2) {
+        case 'N':
+            sym = 'L';
+            break;
+        case 'S':
+            sym = 'F';
+            break;
+        case 'W':
+            sym = '-';
+            break;
+        default:
+            assert(false);
+        }
+    } else if (c1 == 'N') {
+        switch (c2) {
+        case 'S':
+            sym = '|';
+            break;
+        case 'W':
+            sym = 'J';
+            break;
+        default:
+            assert(false);
+        }
+    } else if (c1 == 'S') {
+        assert(c2 == 'W');
+        sym = '7';
+    }
+    map[s.first + s.second * width] = sym;
+}
+
 void part2() {
-    std::ifstream ifs("../../10/input.txt");
+    const auto& [omap, start, width, height] = parseInput();
+    Map map;
+    map.resize(omap.size(), '.');
+    map[start.first + start.second * width] = 'S';
+    auto prev = start;
+    Vec next = first(omap, start, width, height);
+    char c = omap[next.first + next.second * width];
+    while (c != 'S') {
+        map[next.first + next.second * width] = c;
+        auto tmp = next;
+        move(omap, width, prev, next);
+        prev = tmp;
+        c = omap[next.first + next.second * width];
+    }
+    int32_t cnt{};
+    replaceS(map, start, width, height);
+    for (int y = 0; y < height; y++) {
+        bool inside = false;
+        char edgeStart{};
+
+        for (int x = 0; x < width; x++) {
+            c = map[x + y * width];
+            if (c == 'L' || c == 'F') {
+                edgeStart = c;
+            } else if (edgeStart != 0 && c == 'J' || c == '7') {
+                switch (edgeStart) {
+                case 'L':
+                    inside = c == 'J' == inside;
+                    break;
+                case 'F':
+                    inside = c == '7' == inside;
+                    break;
+                default:
+                    assert(false);
+                }
+                edgeStart = 0;
+            } else {
+                if (edgeStart == 0) {
+                    if (c != '.')
+                        inside = !inside;
+                    if (c == '.' && inside)
+                        cnt++;
+                }
+            }
+        }
+    }
+    std::cout << cnt << std::endl;
 }
 
 int main() {
